3de9f6681556d6bcbae67e172d320b2e7c01f7f8
Daniel Machado <daniel.machado.av@gmail.com>
2020-07-14 00:05:11 -0300

    Acrescentei uma string a ser impressa na tela do console

--- EoqdoLearningGit/Program.cs
+++ EoqdoLearningGit/Program.cs
@@ -7,6 +7,8 @@ namespace EoqdoLearningGit
         static void Main(string[] args)
         {
             Console.WriteLine("\n\n   Como assim mlk?");
+
+            Console.WriteLine("\n\n   Denovo, man?");
         }
     }
 }
