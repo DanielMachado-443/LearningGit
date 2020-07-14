67bac41777f69348d3123c7787ca7d0ebcdb198e
Daniel Machado <daniel.machado.av@gmail.com>
2020-07-14 00:02:02 -0300

    Projeto que imprime mensagem na tela do console

---
+++ EoqdoLearningGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EoqdoLearningGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n   Como assim mlk?");
+        }
+    }
+}
